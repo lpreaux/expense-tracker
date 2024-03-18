78303326c2697228af892662b80235f243642300
Lucas Préaux <contact@lucaspreaux.fr>
2024-03-18 16:20:00 +0100

    feat(expenses): :sparkles: Add PWA features - WORKS OFFLINE !!

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,17 @@
-import { ApplicationConfig } from "@angular/core";
+import { ApplicationConfig, isDevMode } from "@angular/core";
 import { provideRouter } from "@angular/router";
 
 import { routes } from "./app.routes";
 import { DB_CONFIG, dbConfig } from "./db.config";
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     { provide: DB_CONFIG, useValue: dbConfig },
-  ],
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+],
 };
